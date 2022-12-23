cf9926313725a797f2bea214f0c27b27007e475b
Kristian Gunder Kramås <kristiankramas@outlook.com>
2022-12-23 09:37:43 +0100

    Added trpc after autoremove

---
+++ apps/next/pages/api/trpc/[trpc].tsx
@@ -0,0 +1,28 @@
+import { appRouter, createContext } from "@acme/api";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError:
+    process.env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(`❌ tRPC failed on ${path}: ${error}`);
+        }
+      : undefined,
+});
+
+// If you need to enable cors, you can do so like this:
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   // Enable cors
+//   await cors(req, res);
+
+//   // Let the tRPC handler do its magic
+//   return createNextApiHandler({
+//     router: appRouter,
+//     createContext,
+//   })(req, res);
+// };
+
+// export default handler;
